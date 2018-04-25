beed0f62623b857b8aafea19de386ac027d710b6
CallMeDDL <liuty1317@gmail.com>
2018-04-25 14:03:09 -0700

    Add movie infor

---
+++ project1/project1C/movie_info.php
@@ -0,0 +1,148 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+		<title>Show Movie Information</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+
+		<!-- link to Bootstrap -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+</head>
+<body>
+	<header class="blog-header py-3 bg-warning text-center">
+		<a class="blog-header-logo text-white">
+			<h1>Movie Databse Query Interface</h1>
+		</a>
+	</header>
+	<div class="container-fluid">
+		<div class="row">
+			<nav class="col-md-3 d-none d-md-block bg-light sidebar">
+				<br>
+				<div class="sidebar-sticky">
+					<ul class="nav flex-column">
+						<h5>Input Option</h5>
+						<li class="nav-item">
+							<a class="nav-link  text-warning" href="add_people.php">Add an Actor or Director</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link text-warning" href="add_movie.php">Add Movie Information</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link text-warning" href="search_movie.php">Add Movie Comment</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link text-warning" href="search_movie.php">Add Movie and Actor Relation</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link text-warning" href="search_movie.php">Add Movie and Director Relation</a>
+						</li>
+					</ul>
+				</div>
+				<br><br>
+				<div class="sidebar-sticky">
+					<ul class="nav flex-column">
+						<h5>Browsing Option</h5>
+						<li class="nav-item">
+							<a class="nav-link  text-warning" href="search_actor.php">Show Actor Information</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link  text-warning" href="search_movie.php">Show Movie Information</a>
+						</li>
+					</ul>
+				</div>
+			</nav>
+			<div class="col-9">
+				<br><br>
+				<div class="container">
+					<div class="row">
+						<div class="col-1"></div>
+						<div class="col-10">
+							<h2>Show Movie Information</h2>
+							<hr style="width: 100%; color: black; height: 1px;" />
+						</div>
+					</div>
+				</div>
+				<div class="container">
+					<div class="row">
+						<div class="col-2"></div>
+						<div class="col-10">
+							<h5>Movie Information:</h5>
+						</div>
+					</div>
+				</div>
+				<div class="container">
+					<div class="row">
+						<div class="col-2"></div>
+						<div class="col-10">
+							<?php
+								echo "test"
+							?>
+						</div>
+					</div>
+				</div>
+				<br><br>
+				<div class="container">
+					<div class="row">
+						<div class="col-2"></div>
+						<div class="col-10">
+							<h5>Actors in the Movie:</h5>
+						</div>
+					</div>
+				</div>
+				<div class="container">
+					<div class="row">
+						<div class="col-2"></div>
+						<div class="col-10">
+							<?php
+								echo "test"
+							?>
+						</div>
+					</div>
+				</div>
+				<br>
+				<div class="container">
+					<div class="row">
+						<div class="col-2"></div>
+						<div class="col-2">
+							<input type="submit" class="btn btn-outline-warning btn-block" value="Add Actor"> 
+						</div>
+						<div class="col-1"></div>
+						<div class="col-2">
+							<input type="submit" class="btn btn-outline-warning btn-block" value="Add Director"> 
+						</div>
+					</div>
+				</div>
+				<br><br>
+				<div class="container">
+					<div class="row">
+						<div class="col-2"></div>
+						<div class="col-10">
+							<h5>Movie Comment:</h5>
+						</div>
+					</div>
+				</div>
+				<div class="container">
+					<div class="row">
+						<div class="col-2"></div>
+						<div class="col-10">
+							<?php
+								echo "test"
+							?>
+						</div>
+					</div>
+				</div>
+				<br>
+				<div class="container">
+					<div class="row">
+						<div class="col-2"></div>
+						<div class="col-2">
+							<input type="submit" class="btn btn-outline-warning btn-block" value="Add Comment"> 
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+</body>
+</html>
