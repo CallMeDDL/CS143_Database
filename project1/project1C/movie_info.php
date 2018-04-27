a46bdebec024838467fddfa6e62f3c8f9ffe1bbd
Yufeng <35382018+MagicF666@users.noreply.github.com>
2018-04-26 18:51:36 -0700

    Update movie_info.php

--- project1/project1C/movie_info.php
+++ project1/project1C/movie_info.php
@@ -75,7 +75,41 @@
 						<div class="col-2"></div>
 						<div class="col-10">
 							<?php
-								echo "test"
+								$db = new mysqli('localhost', 'cs143', '', 'CS143');
+								if($db->connect_errno > 0){
+								die('Unable to connect to database [' . $db->connect_error . ']');
+								}
+
+								$id = 3563;//$_GET[""];
+								//$title = $_GET[""];
+								//$year = $_GET[""];
+								$query1 = ("SELECT rating, company, year, title 
+										   FROM Movie 
+										   WHERE id = $id
+										   ORDER BY rating, company, year, title;
+										   ");
+
+								$query2 = ("SELECT genre 
+										    FROM MovieGenre 
+										    WHERE mid = $id 
+										    ORDER BY genre;
+										   ");
+								$rs1 = $db->query($query1);
+								$rs2 = $db->query($query2);
+
+								while($row = mysqli_fetch_row($rs1)) {
+									$rating = $row[0];
+									$company = $row[1];
+									$year = $row[2];
+									$title = $row[3];
+								}
+								while($row = mysqli_fetch_row($rs2)) {
+									$genre = $row[0];
+								}
+								echo "Title: ".$title."( ".$year." )"."<br>";
+								echo "Producer: ".$company."<br>";
+								echo "MPAA Rating: ".$rating."<br>";
+								echo "Genre: ".$genre."<br>";
 							?>
 						</div>
 					</div>
@@ -94,7 +128,42 @@
 						<div class="col-2"></div>
 						<div class="col-10">
 							<?php
-								echo "test"
+								$db = new mysqli('localhost', 'cs143', '', 'CS143');
+								if($db->connect_errno > 0){
+								die('Unable to connect to database [' . $db->connect_error . ']');
+								}
+
+								$id = 3563;//$_GET[""];
+								//$title = $_GET[""];
+								//$year = $_GET[""];
+								$query3 = ("SELECT first, last, role
+										   FROM Actor 
+										   INNER JOIN MovieActor ON Actor.id = MovieActor.aid
+										   WHERE MovieActor.mid = $id
+										   ORDER BY last, first, role;
+										   ");
+								// $query4 = ("SELECT A.first, A.last, A.dob, A.dod
+								// 		   FROM Director A 
+								// 		   INNER JOIN MovieDirector B ON A.id = B.did
+								// 		   WHERE B.mid = $id
+								// 		   ORDER BY A.last, A.first, A.dob;
+								// 		   ");
+								$rs3 = $db->query($query3);
+								// $rs4 = $db->query($query4);
+								while($row = mysqli_fetch_row($rs3)) {
+									$first1 = $row[0];
+									$last1 = $row[1];
+									$role  = $row[2];
+									echo "Name: ".$first1." ".$last1."<br>";
+									echo "Role: ".$role."<br>";
+								}
+								// while($row = mysqli_fetch_row($rs4)) {
+								// 	$first2 = $row[0];
+								// 	$last2 = $row[1];
+								// 	$dob2 = $row[2];
+								//	$dod2 = $row[3];
+								//echo "Name: ".$first1." ".$last1."<br>";
+								//echo "Role: ".$role."<br>";	
 							?>
 						</div>
 					</div>
@@ -130,7 +199,26 @@
 						<div class="col-2"></div>
 						<div class="col-10">
 							<?php
-								echo "test"
+								$db = new mysqli('localhost', 'cs143', '', 'CS143');
+								if($db->connect_errno > 0){
+								die('Unable to connect to database [' . $db->connect_error . ']');
+								}
+								$id = 3563;//$_GET[""];
+								$query4 = ("SELECT name, rating, comment 
+											FROM Review 
+											WHERE mid = $id;"
+										  );
+								$rs4 = $db->query($query4);
+
+								while($row = mysqli_fetch_row($rs4)) {
+									$name4 = $row[0];
+									$rating4 = $row[1];
+									$commnet4 = $row[2];
+								}
+								echo "Name: ".$name4."<br>";
+								echo "Rating: ".$rating4."<br>";
+								echo "Comment: ".$cpmment4."<br>";
+							
 							?>
 						</div>
 					</div>
