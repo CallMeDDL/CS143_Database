ffd6a4427b08d770042a8c855f9bee4a1c7d98d0
Yufeng <35382018+MagicF666@users.noreply.github.com>
2018-06-02 09:48:18 -0700

    Update reddit_model.py

--- project2/project2B/reddit_model.py
+++ project2/project2B/reddit_model.py
@@ -1,8 +1,95 @@
 from __future__ import print_function
 from pyspark import SparkConf, SparkContext
 from pyspark.sql import SQLContext
+import re
+import string
+import argparse
+from pyspark.sql.functions import udf, col
+from pyspark.sql.types import LongType, StringType
 
 # IMPORT OTHER MODULES HERE
+def sanitize1(test):
+    text = text.replace("\t", " ")
+    text = text.replace("\n", " ")
+    
+    # remove url as [some text](http://ucla.edu)
+    text = re.sub(r'\[.*?\]\((https?|ftp):\/\/(-\.)?([^\s/?\.#-]+\.?)+(\/[^\s]*)?\)','',text)
+    text = re.sub (r'(https?|ftp):\/\/(-\.)?([^\s/?\.#-]+\.?)+(\/[^\s]*)?','',text)
+    text = re.sub (r'\[.*?\]\(.*?\)','',text)
+   
+    text_list1 = text.split(" ")
+    text_list1 = list(filter(None, text_list1))
+
+    punctuation_allowed = ['.', '!', '?', ',', ';', ':']
+    text_list2 = list()
+    while text_list1:
+
+        # initialization
+        text_cur = text_list1.pop(0)
+        l = 0
+        r = len(text_cur) - 1
+
+        # parse the left part
+        text_new_left = list()
+        while l <= r:
+            if text_cur[l] in string.punctuation and text_cur[l] != '$':
+                if text_cur[l] in punctuation_allowed:
+                    text_new_left.append(text_cur[l])
+                l += 1
+            else:
+                break
+
+        # parse the right part
+        text_new_right = list()
+        while l <= r:
+            if text_cur[r] in string.punctuation and text_cur[r] != '$':
+                if text_cur[r] in punctuation_allowed:
+                    text_new_right.insert(0, text_cur[r])
+                r -= 1
+            else:
+                break
+
+        # parse the middle part
+        if l <= r:
+            text_new_mid = text_cur[l:r+1]
+        else:
+            text_new_mid = ""
+
+        # append the result
+        text_list2.extend(text_new_left)
+        if text_new_mid:
+            text_list2.append(text_new_mid)
+        text_list2.extend(text_new_right)
+    
+    # parsed_text
+    parsed_text1 = [item.lower() for item in text_list2]
+    parsed_text = ' '.join(e for e in parsed_text1)
+
+    # Unigrams
+    unigrams1 = []
+    for i in range(len(parsed_text1)):
+        if parsed_text1[i] not in string.punctuation:
+            unigrams1.append(parsed_text1[i])
+    unigrams = ' '.join(f for f in unigrams1)
+
+    # Bigrams
+    bigrams1 = []
+    for i in range(len(parsed_text1) - 1):
+        if parsed_text1[i] not in punctuation_allowed and parsed_text1[i + 1] not in punctuation_allowed:
+            bigrams1.append(parsed_text1[i] + "_" + parsed_text1[i + 1])
+    bigrams = ' '.join(e for e in bigrams1)
+
+    # Trigrams
+    trigrams1 = []
+    for i in range(len(parsed_text1) - 2):
+        if parsed_text1[i] not in punctuation_allowed and parsed_text1[i + 1] not in punctuation_allowed and parsed_text1[i + 2] not in punctuation_allowed:
+            trigrams1.append(parsed_text1[i] + "_" + parsed_text1[i + 1] + "_" + parsed_text1[i + 2])
+    trigrams = ' '.join(e for e in trigrams1)
+    
+    comments = []
+    comments = unigrams1 + bigrams1 + trigrams1
+    comm = ' '.join(str(e) for e in comments)
+    return comm
 
 
 def main(context):
@@ -19,6 +106,11 @@ def main(context):
     df_lab.createOrReplaceTempView('view_lab')
     df_com = df_com.where('id IN (SELECT Input_id FROM view_lab)')
     # df_com.describe().show()
+    
+    # Task 4 & 5:
+    sanitize_udf = udf(sanitize1, StringType())
+    df_sanitize = df_com.select("id", sanitize_udf(col("body")).alias("comments"))
+    #df_sanitize.show()
 
 
 if __name__ == "__main__":
