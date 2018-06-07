46e0388f78fedcbd6e1a906a6a7868091b8f503d
IvyHan <ivyhan2013@126.com>
2018-06-06 19:06:22 -0700

    Finish task 10

--- project2/project2B/reddit_model.py
+++ project2/project2B/reddit_model.py
@@ -11,7 +11,13 @@ from pyspark.ml.classification import LogisticRegression
 from pyspark.ml.classification import LogisticRegressionModel
 from pyspark.ml.tuning import CrossValidator, ParamGridBuilder,CrossValidatorModel
 from pyspark.ml.evaluation import BinaryClassificationEvaluator
-
+states = ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California', 'Colorado', 'Connecticut',\
+     'Delaware', 'District of Columbia', 'Florida', 'Georgia', 'Hawaii', 'Idaho', 'Illinois', \
+     'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana', 'Maine', 'Maryland', 'Massachusetts', \
+     'Michigan', 'Minnesota', 'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire', \
+     'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'North Dakota', 'Ohio', 'Oklahoma', 'Oregon',\
+      'Pennsylvania', 'Rhode Island', 'South Carolina','South Dakota', 'Tennessee', 'Texas', 'Utah', \
+      'Vermont', 'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming']
 def train_process(pos, neg):
     # Initialize two logistic regression models.
     # Replace labelCol with the column containing the label, and featuresCol with the column containing the features.
@@ -60,7 +66,7 @@ def main(context):
     # YOUR CODE HERE
     # YOU MAY ADD OTHER FUNCTIONS AS NEEDED
 
-    # Task 1:
+    Task 1:
    
 
     
@@ -68,7 +74,7 @@ def main(context):
     df_sub = sqlContext.read.json("/home/cs143/data/submissions.json.bz2")
     df_lab = sqlContext.read.csv("labeled_data.csv", header=True, inferSchema=True)
     
-    # save data
+    # #save data
     
     # df_com.write.parquet("/home/cs143/data/comments-minimal.parquet")
     # df_sub.write.parquet("/home/cs143/data/submissions.parquet")
@@ -125,36 +131,39 @@ def main(context):
     df_neg = sqlContext.sql('SELECT id, features, view_neglab.label AS label FROM view_cv INNER JOIN view_neglab ON view_neglab.Input_id = view_cv.id')
 
     # Task 7
-    df_pos.write.parquet("home/cs143/data/pos.parquet")
-    df_neg.write.parquet("home/cs143/data/neg.parquet")
+    # df_pos.write.parquet("home/cs143/data/pos.parquet")
+    # df_neg.write.parquet("home/cs143/data/neg.parquet")
     train_process(df_pos, df_neg)
 
-    # Task 8
+    # # Task 8
     idtype_udf = udf(idtype, StringType())
-    df_com_full = df_com.select("id",idtype_udf(col("link_id")).alias("link_id"),"body","created_utc","author_flair_text")
+    df_com_full = df_com.select("id",idtype_udf(col("link_id")).alias("link_id"),"body","created_utc","author_flair_text",col('score').alias('com_score'))
     # df_com_full.write.parquet("/home/cs143/data/comfull.parquet")
     # df_com_full = sqlContext.read.parquet("/home/cs143/data/comfull.parquet").sample(False, 0.01, None)
     # df_com_full.show()
 
-    df_sub_full = df_sub.select(col("id").alias("sub_id"), "title")
+    df_sub_full = df_sub.select(col("id").alias("sub_id"), "title",col('score').alias('sub_score'))
     # df_sub_full = sqlContext.read.parquet("/home/cs143/data/subfull.parquet").sample(False, 0.01, None)
     # df_sub_full.show()
     # df_sub_full.write.parquet("/home/cs143/data/subfull.parquet")
 
 
     df_full = df_com_full.join(df_sub_full, df_com_full.link_id == df_sub_full.sub_id, 'inner')
-    df_full = df_full.select(col('id'), col('link_id'), col('created_utc'), col('body'), col('author_flair_text'), col('title'))
+    df_full = df_full.select(col('id'), col('link_id'), col('created_utc'), col('body'), col('author_flair_text'), col('title'),col('com_score'),col('sub_score'))
     # df_full.show()
     # df_full.write.parquet("/home/cs143/data/full.parquet")
     # df_full.show()
 
-    # Task 9
+    # df_full = sqlContext.read.parquet("/home/cs143/data/full.parquet")
+
+    # # Task 9
+    df_full = df_full.sample(False, 0.5, None)
     df_full = df_full.where("body not like '&gt%'")
     df_full = df_full.where("body not like '%/s%'")
     # df_full.show()
 
 
-    df_full_sanitize = df_full.select("id", sanitize_udf(col("body")).alias("comments"),col('link_id'), col('created_utc'),col('author_flair_text'), col('title'))
+    df_full_sanitize = df_full.select("id", sanitize_udf(col("body")).alias("comments"),col('link_id'), col('created_utc'),col('author_flair_text'), col('title'),col('com_score'),col('sub_score'))
     # df_full_sanitize.show()
 
 
@@ -173,33 +182,55 @@ def main(context):
     # posResult = sqlContext.read.parquet("/home/cs143/data/posResult.parquet")
     # negResult = sqlContext.read.parquet("/home/cs143/data/negResult.parquet")
 
-  
     posthreshold_udf = udf(set_pos_threshold,IntegerType())
     negthreshold_udf = udf(set_neg_threshold,IntegerType())
 
 
-    posResult = posResult.select("id",posthreshold_udf(col('probability')).alias("probability"),col('link_id'),col('created_utc'),col('author_flair_text'), col('title'))
-    negResult = negResult.select("id",negthreshold_udf(col('probability')).alias("probability"), col('link_id'), col('created_utc'),col('author_flair_text'), col('title'))
+    posResult = posResult.select("id",posthreshold_udf(col('probability')).alias("probability"),col('link_id'),col('created_utc'),col('author_flair_text'), col('title'),col('com_score'),col('sub_score'))
+    negResult = negResult.select("id",negthreshold_udf(col('probability')).alias("probability"), col('link_id'), col('created_utc'),col('author_flair_text'), col('title'),col('com_score'),col('sub_score'))
 
     # Task 10
-    num_rows = posResult.count()
-    num_pos =  posResult.filter(posResult.probability == 1).count()
-    num_neg =  negResult.filter(negResult.probability == 1).count()
-    print('percentage of positive %f'%(num_pos / num_rows))
-    print('Percentage of negative %f'%(num_neg / num_rows))
 
-    sub_pos_pct = posResult.groupBy('link_id','title').agg((sum('probability') / count('probability')).alias('pos_pct'))
-    sub_neg_pct = negResult.groupBy('link_id','title').agg((sum('probability') / count('probability')).alias('neg_pct'))
+    # 1. across submission
+    sub_pos_pct = posResult.groupBy('title').agg((sum('probability') / count('probability')).alias('pos_pct')).orderBy('title')
+    sub_neg_pct = negResult.groupBy('title').agg((sum('probability') / count('probability')).alias('neg_pct')).orderBy('title')
 
-    time_pos_pct = posResult.groupBy('created_utc').agg((sum('probability') / count('probability')).alias('pos_pct'))
-    time_neg_pct = negResult.groupBy('created_utc').agg((sum('probability') / count('probability')).alias('neg_pct'))
-
-    time_pos_pct = time_pos_pct.select(from_unixtime(col('created_utc')).alias('time'),'pos_pct').show()
-    time_neg_pct = time_neg_pct.select(from_unixtime(col('created_utc')).alias('time'),'neg_pct').show()
+    sub_pos_pct.coalesce(1).write.mode("overwrite").format("com.databricks.spark.csv").option("header", "true").csv("data/sub_pos_pct.csv")
+    sub_neg_pct.coalesce(1).write.mode("overwrite").format("com.databricks.spark.csv").option("header", "true").csv("data/sub_neg_pct.csv")
 
 
+    # 2. across time
+    time_pos_pct = posResult.groupBy(from_unixtime(col('created_utc')).cast(DateType()).alias('time')).agg((sum('probability') / count('probability')).alias('pos_pct')).orderBy('time')
+    time_neg_pct = negResult.groupBy(from_unixtime(col('created_utc')).cast(DateType()).alias('time')).agg((sum('probability') / count('probability')).alias('neg_pct')).orderBy('time')
+
+   
+    time_pos_pct.coalesce(1).write.mode("overwrite").format("com.databricks.spark.csv").option("header", "true").csv("data/time_pos_pct.csv")
+    time_neg_pct.coalesce(1).write.mode("overwrite").format("com.databricks.spark.csv").option("header", "true").csv("data/time_neg_pct.csv")
+
+    #3. across state
+    state_pos_pct = posResult.groupBy('author_flair_text').agg((sum('probability') / count('probability')).alias('pos_pct'))
+    state_neg_pct = negResult.groupBy('author_flair_text').agg((sum('probability') / count('probability')).alias('neg_pct'))
+    df_state = sqlContext.createDataFrame(states, StringType())
+    state_pos_pct = state_pos_pct.join(df_state, state_pos_pct.author_flair_text == df_state.value, 'inner')
+    state_pos_pct = state_pos_pct.select(col('author_flair_text').alias('state'), col('pos_pct')).orderBy('state')
+
+    state_neg_pct = state_neg_pct.join(df_state, state_neg_pct.author_flair_text == df_state.value, 'inner')
+    state_neg_pct = state_neg_pct.select(col('author_flair_text').alias('state'), col('neg_pct')).orderBy('state')
+
+    state_pos_pct.coalesce(1).write.mode("overwrite").format("com.databricks.spark.csv").option("header", "true").csv("data/state_pos_pct.csv")
+    state_neg_pct.coalesce(1).write.mode("overwrite").format("com.databricks.spark.csv").option("header", "true").csv("data/state_neg_pct.csv")
 
 
+    #4. comment score and submission score
+    com_score_pos_pct = posResult.groupBy('com_score').agg((sum('probability') / count('probability')).alias('pos_pct')).orderBy('com_score')
+    com_score_neg_pct = negResult.groupBy('com_score').agg((sum('probability') / count('probability')).alias('neg_pct')).orderBy('com_score')
+    sub_score_pos_pct = posResult.groupBy('sub_score').agg((sum('probability') / count('probability')).alias('pos_pct')).orderBy('sub_score')
+    sub_score_neg_pct = negResult.groupBy('sub_score').agg((sum('probability') / count('probability')).alias('neg_pct')).orderBy('sub_score')
+
+    com_score_pos_pct.coalesce(1).write.mode("overwrite").format("com.databricks.spark.csv").option("header", "true").csv("data/com_score_pos_pct.csv")
+    com_score_neg_pct.coalesce(1).write.mode("overwrite").format("com.databricks.spark.csv").option("header", "true").csv("data/com_score_neg_pct.csv")
+    sub_score_pos_pct.coalesce(1).write.mode("overwrite").format("com.databricks.spark.csv").option("header", "true").csv("data/sub_score_pos_pct.csv")
+    sub_score_neg_pct.coalesce(1).write.mode("overwrite").format("com.databricks.spark.csv").option("header", "true").csv("data/sub_score_neg_pct.csv")
 
 
 def idtype(id):
